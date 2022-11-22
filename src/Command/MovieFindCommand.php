baddf204b4019357e1e7f390c793fa299fd34b5d
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-11-22 17:34:10 +0100

    Console v2

--- src/Command/MovieFindCommand.php
+++ src/Command/MovieFindCommand.php
@@ -21,6 +21,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class MovieFindCommand extends Command
 {
     private ?SymfonyStyle $io = null;
+    private ?string $type = null;
+    private ?string $value = null;
 
     public function __construct(
         private MovieRepository $movieRepository,
@@ -38,35 +40,41 @@ class MovieFindCommand extends Command
         ;
     }
 
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+        $this->provider->setSymfonyStyle($this->io);
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->value = $input->getArgument('value')) {
+            $this->value = $this->io->ask('What is the title or id of the movie you\'re searching for?');
+        }
+
+        $this->type = strtolower($input->getArgument('type'));
+        if (\in_array($this->type, ['title', 'id'])) {
+            $this->type = substr($this->type, 0, 1);
+        }
+        if (!\in_array($this->type, ['t', 'i'])) {
+            $this->type = $this->io->choice('What type of data are your searching on?', ['t' => 'title', 'i' => 'id'], 't');
+        }
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->io = $io = new SymfonyStyle($input, $output);
-        $this->provider->setSymfonyStyle($io);
-
-        if (!$value = $input->getArgument('value')) {
-            $value = $io->ask('What is the title or id of the movie you\'re searching for?');
-        }
-
-        $type = strtolower($input->getArgument('type'));
-        if (\in_array($type, ['title', 'id'])) {
-            $type = substr($type, 0, 1);
-        }
-        if (!\in_array($type, ['t', 'i'])) {
-            $type = $io->choice('What type of data are your searching on?', ['t' => 'title', 'i' => 'id'], 't');
-        }
-
-        $io->title('Your search :');
-        $io->text(sprintf("Searching for a movie with %s=%s", $type, $value));
-        if (OMDbApiConsumer::MODE_ID === $type && $movie = $this->movieRepository->findOneBy(['imdbId' => $value])) {
+        $this->io->title('Your search :');
+        $this->io->text(sprintf("Searching for a movie with %s=%s", $this->type, $this->value));
+        if (OMDbApiConsumer::MODE_ID === $this->type && $movie = $this->movieRepository->findOneBy(['imdbId' => $this->value])) {
             $this->displayResult($movie);
 
             return Command::SUCCESS;
         }
 
         try {
-            $movie = $this->provider->getMovie($type, $value);
+            $movie = $this->provider->getMovie($this->type, $this->value);
         } catch (NotFoundHttpException) {
-            $io->error('Movie not found!');
+            $this->io->error('Movie not found!');
 
             return Command::FAILURE;
         }
