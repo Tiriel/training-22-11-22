b73badb467f6c2bb9ae7b8be01ebd53074998d70
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-11-24 14:44:40 +0100

    Mocks 1

---
+++ tests/Provider/GenreProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Provider;
+
+use App\Entity\Genre;
+use App\Provider\GenreProvider;
+use App\Repository\GenreRepository;
+use App\Transformer\OmdbGenreTransformer;
+use PHPUnit\Framework\TestCase;
+
+class GenreProviderTest extends TestCase
+{
+    public function testProviderReturnsGenerator(): void
+    {
+        $transformer = $this->getMockBuilder(OmdbGenreTransformer::class)
+            ->disallowMockingUnknownTypes()
+            ->onlyMethods(['transform'])
+            ->getMock()
+            ;
+        $transformer->expects($this->exactly(2))->method('transform')->willReturn(new Genre());
+
+        $mockRepository = $this->getMockBuilder(GenreRepository::class)
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->onlyMethods(['findOneBy'])
+            ->getMock()
+            ;
+        $mockRepository->expects($this->exactly(2))->method('findOneBy')->willReturn(null);
+        $provider = new GenreProvider($transformer, $mockRepository);
+
+        $genres = $provider->getGenresFromString('Foo, Bar');
+        foreach ($genres as $genre) {
+            $this->assertInstanceOf(Genre::class, $genre);
+        }
+        $this->assertInstanceOf(\Generator::class, $genres);
+    }
+}
